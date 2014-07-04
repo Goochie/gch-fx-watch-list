4d7bc53d2efa8bf2120a879b45a3c34403aed55f
Bill Gooch <billgch@gmail.com>
2014-07-04 12:35:44 +0100

    Added test spec's

---
+++ tests/e2e/gch-fx-watch-listSpec.js
@@ -0,0 +1,18 @@
+/**
+ * Author : Bill Gooch
+ *
+ * e2e tests for market watch widget
+ *
+ */
+
+describe('Market watch widget :: ', function(){
+
+    beforeEach(module('billGchPorfolioApp'));
+
+    it('The expected columns are not visible ', angular.mock.inject(function( someDependency){
+
+
+
+    }));
+
+})
---
+++ tests/unit/gch-fx-watch-listSpec.js
@@ -0,0 +1,30 @@
+/**
+ * Author : Bill Gooch
+ *
+ * Unit tests for market watch widget
+ *
+ */
+
+describe('Market watch widget :: ', function(){
+
+    beforeEach(module('billGchPorfolioApp'));
+
+    it('RateSelected event has not been fired', angular.mock.inject(function( someDependency){
+
+
+
+    }));
+
+    it('The expected columns are not visible ', angular.mock.inject(function( someDependency){
+
+
+
+    }));
+
+
+
+})
+
+
+
+
