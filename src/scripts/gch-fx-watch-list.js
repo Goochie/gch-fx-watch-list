0124e3b6eb3b426205420d4aae0b4470474bce9f
Bill Gooch <billgch@gmail.com>
2014-07-25 10:11:11 +0100

    Changed styling to remove dependency from bootstrap

--- src/scripts/gch-fx-watch-list.js
+++ src/scripts/gch-fx-watch-list.js
@@ -12,14 +12,12 @@ angular.module('fxMarketWatchModule',['btford.socket-io']);
 
     // Directive
     //---------------------------------
-    function fxMarketWatchDirective (fxPriceService){
+    function fxMarketWatchDirective (fxPriceService,SYMBOL_SELECTED_EVT){
 
         function Controller ($scope, SYMBOL_SELECTED_EVT){
 
-            //EVENT HANDLERS
-            //----------------------------------------------------
-
             $scope.symbolSelected = function(currency){
+
                 $scope.$emit(SYMBOL_SELECTED_EVT,currency);
 
             };
@@ -80,19 +78,19 @@ angular.module('fxMarketWatchModule',['btford.socket-io']);
 
     function wacthlistTemplate(){
 
-        var tpl = '<div id="gchFxMarketWatch">'+
+        var tpl = '<div id="gchFxMarketWatch" >'+
                     '<div>'+
                         '<div class="col-1-4Header">Symbol</div>'+
                         '<div class="col-1-4Header">Bid</div>'+
                         '<div class="col-1-4Header">Ask</div>'+
                         '<div class="col-1-4Header">Time</div>'+
                     '</div>'+
-                    '<div ng-repeat="rate in rates" ng-click="symbolSelected(rate)">'+
+                    '<div class="symbol" ng-repeat="rate in rates" ng-click="symbolSelected(rate)">'+
                         '<div class="col-1-4">{{rate.symbol}}</div>'+
                         '<div class="col-1-4">{{rate.bidBig}}<span class="fx-point">{{rate.bidPoint}}</span>'+
-                        '<span class="glyphicon" ng-class="{\'glyphicon-arrow-down fx-priceDown\' : rate.bidBullBear == \'bear\', \'glyphicon-arrow-up fx-priceUp\' : rate.bidBullBear == \'bull\'}"></span></div>'+
+                        '<span  ng-class="{\'fa fa-arrow-down fx-priceDown\' : rate.bidBullBear == \'bear\', \'fa fa-arrow-up fx-priceUp\' : rate.bidBullBear == \'bull\' , \'fa fa-arrows-h fx-noChange\' : rate.bidBullBear == \'0\' }"></span></div>'+
                         '<div class="col-1-4">{{rate.offerBig}}<span class="fx-point">{{rate.offerPoint}}</span>'+
-                        '<span class="glyphicon" ng-class="{\'glyphicon-arrow-down fx-priceDown\' : rate.offerBullBear == \'bear\', \'glyphicon-arrow-up fx-priceUp\' : rate.offerBullBear == \'bull\'}"></span></div>'+
+                        '<span  ng-class="{\'fa fa-arrow-down fx-priceDown\' : rate.offerBullBear == \'bear\', \'fa fa-arrow-up fx-priceUp\' : rate.offerBullBear == \'bull\', \'fa fa-arrows-h fx-noChange\' : rate.bidBullBear == \'0\'}"></span></div>'+
                         '<div class="col-1-4">{{rate.timeStamp | date : \'hh:mm:ss\'}}</div>'+
                     '</div>'+
                 '</div>';
